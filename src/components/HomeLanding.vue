a98c742f47fc603fc46bafb373e025cb774d6c3b
Tomas Gil <tomasgil1234@gmail.com>
2021-02-25 21:00:37 -0300

    refactor: remove comments and add .env.example

--- src/components/HomeLanding.vue
+++ src/components/HomeLanding.vue
@@ -10,11 +10,6 @@ import ImageGrid from "./ImageGrid";
 import { getToken } from "../service";
 import { useStore } from "vuex";
 
-//data.pictures
-//data.hasMore
-// cada picture cropped_picture: "http://interview.agileengine.com/pictures/cropped/0002.jpg"
-//id: "90322bc9cd20fd0f4ffa"
-
 export default {
   name: "HomeLanding",
   components: {
